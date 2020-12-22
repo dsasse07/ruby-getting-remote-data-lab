db9e2ace4aa159cea651950cc2d66afe147bb855
Daniel Sasse <dsasse07@gmail.com>
2020-12-21 22:52:15 -0500

    Done

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gem 'rspec'
 gem 'pry'
+gem 'learn-co'
+gem 'awesome_print'
--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+require 'awesome_print'
+require 'pry'
+
+
+class GetRequester
+    attr_accessor :uri, :url
+    def initialize(url)
+        @url = url           
+        @uri = URI.parse(url)
+    end
+
+    def get_response_body
+        response = Net::HTTP.get_response(self.uri)
+        response.body
+    end    
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+end
+
+binding.pry
+0
